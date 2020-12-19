ed38a134a893cbec276f738433ce75dac7dbb126
DRGladwell <david_gladwell@hotmail.com>
2020-12-19 14:49:10 +0000

    Used to configure extraction of features with CNN

---
+++ CNN_TransferLearn_Train/setup/config.py
@@ -0,0 +1,28 @@
+# import the necessary packages
+import os
+
+
+
+# initialize the base path to the *new* directory that will contain
+# our images after computing the training and testing split
+BASE_PATH = "dataset"
+
+# define the names of the training, testing, and validation
+# directories
+TRAIN = "train"
+# TEST = "test"
+# VAL = "valid"
+
+# initialize the list of class label names
+CLASSES = ["notwheel", "wheel"]
+
+# set the batch size
+BATCH_SIZE = 32
+
+# initialize the label encoder file path and the output directory to
+# where the extracted features (in CSV file format) will be stored
+LE_PATH = os.path.sep.join(["output", "le.cpickle"])
+BASE_CSV_PATH = "output"
+
+# set the path to the serialized model after training
+MODEL_PATH = os.path.sep.join(["output", "model.cpickle"])
